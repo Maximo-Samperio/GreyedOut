5bbbed818c32062c5270911f52d3280421044bd2
Maximo Samperio <maximosamperio01@gmail.com>
2023-05-10 12:38:17 -0300

    Added 2D movement and collision

---
+++ Greyed Out/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    float horizontalMove = 0f;
+
+    bool jump = false;  
+
+    public float runSpeed = 40f;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+       horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;    
+        }
+
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+
+
+}
